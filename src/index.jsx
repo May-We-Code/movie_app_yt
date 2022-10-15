8162de7edcd077b643a92095eb78c0c22dbddb1a
May-We-Code <96605037+May-We-Code@users.noreply.github.com>
2022-10-15 11:41:57 +0530

    Revert "Update index.jsx"

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-//Start of the file
 ReactDOM.render(
   <React.StrictMode>
     <App />
