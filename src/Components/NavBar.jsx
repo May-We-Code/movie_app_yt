c8fbbfce03e649257c28c47014089496ebe58b92
Anish <91914231+GoldenPizza1729@users.noreply.github.com>
2023-08-17 01:02:00 +0530

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,12 +2,37 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className='w-full'>
-      <Link to='/'>
-        <div className='text-white text-2xl font-bold md:text-4xl lg:text-5xl ml-10 pt-2'>
-          AZ-Movies
-        </div>
-      </Link>
+    <div className=" w-full bg-black">
+      <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
+        <header className="flex justify-between items-center py-2 md:py-4">
+          <nav className="align-middle hidden lg:flex gap-12 ">
+            <Link
+              to="/"
+              className="text-white hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100"
+            >
+              Home
+            </Link>
+            <Link
+              to="/chat"
+              className="text-white hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100"
+            >
+              Movies
+            </Link>
+            <Link
+              to="/previousorders"
+              className="text-white hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100"
+            >
+              Orders
+            </Link>
+            <Link
+              to="/about"
+              className="text-white hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100"
+            >
+              About
+            </Link>
+          </nav>
+        </header>
+      </div>
     </div>
   );
 }
